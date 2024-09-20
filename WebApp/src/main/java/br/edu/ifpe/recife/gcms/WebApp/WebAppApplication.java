a2441e5f49e64cac0aafb3c8c3fa385a266e4d2f
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2024-09-20 20:38:25 -0300

    Quebrando o código para pratica no jenkins

--- WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplication.java
@@ -1,9 +1,4 @@
-package br.edu.ifpe.recife.gcms.WebApp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class WebAppApplication {
 
 	public static void main(String[] args) {

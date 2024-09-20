4ac3316db1923f4d65a92d8004b82a36c303efcd
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2024-09-20 20:43:32 -0300

    Normalizando o WebAppApplication.java

--- WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplication.java
@@ -1,4 +1,8 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class WebAppApplication {
 
 	public static void main(String[] args) {
